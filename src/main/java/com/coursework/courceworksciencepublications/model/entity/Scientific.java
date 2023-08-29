214c9e3c48d27c47d60d5acb28beffbb702ec525
KripDo <danik2003235@mail.ru>
2023-08-29 21:41:10 +0300

    add strategy to id

--- src/main/java/com/coursework/courceworksciencepublications/model/entity/Scientific.java
+++ src/main/java/com/coursework/courceworksciencepublications/model/entity/Scientific.java
@@ -1,6 +1,8 @@
 package com.coursework.courceworksciencepublications.model.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -13,6 +15,7 @@ import java.time.LocalDate;
 public class Scientific {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String name;
